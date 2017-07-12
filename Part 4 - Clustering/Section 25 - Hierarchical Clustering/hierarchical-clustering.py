19ef1c732c1a6b1a8d73a8eda4fcfabe12d70aca
Moinul <moinulkuet@gmail.com>
2017-07-12 17:31:20 +0200

    Adding Clustering algorithms

---
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical-clustering.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 15:50:56 2017
+
+@author: malmamun
+"""
+
+# import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# get data
+dataset = pd.read_csv('Mall_Customers.csv')
+# Define X, y - independent and dependent variable
+X = dataset.iloc[:, [3,4]].values
+
+# import scipy hierarchical clustering method
+# draw a dendogram to find optimal cluster numbers
+import scipy.cluster.hierarchy as sch
+dendogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+# 5 clusters
+
+# fitting Hierarchical clustering to data set
+from sklearn.cluster.hierarchical import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+y_hc = hc.fit_predict(X)
+
+# visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+
+
+
+                
